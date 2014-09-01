e31036e6140fb874bc992ecb82eee0201012b259
Vinod CG <vini@live.in>
2014-09-01 11:34:33 +0530

    Revert "upgrade sdk to 3.3"
    This reverts commit 5254eadf7c79fe0cb110c7fb92593e1f8c565699.

--- js/hasoffers.js
+++ js/hasoffers.js
@@ -1,7 +1,3 @@
-/* https://mobileapptracking.com
- * Installations and opens will be tracked automatically
- */
-
 function pluginSend(evt, params) {
 	NATIVE && NATIVE.plugins && NATIVE.plugins.sendEvent &&
 		NATIVE.plugins.sendEvent("HasoffersPlugin", evt,
@@ -9,42 +5,35 @@ function pluginSend(evt, params) {
 }
 
 var Hasoffers = Class(function () {
-	this.init = function(){
+	this.init = function(opts){
 		logger.log("{hasoffers} Registering for events on startup");
 	}
 
-	this.setUserIds = function (params) {
-		// Allowed params
-		// uid : custom user id
-		// fb_id: facebook user ID
-		// google_id: Google ID if login is based on google
-		// twitter_id: Twitter ID
-		pluginSend("setUserIds", params);
+	this.trackInstall = function(oldClient){
+		logger.log("Tracking install for "+oldClient+" user.");
+
+		var params = {"userType":(oldClient)?"old":"new"};
+
+		pluginSend("trackInstall", params);
 	}
 
-	this.trackPurchase = function(recieptId, sku, name, price, quantity, currency){
-		quantity = quantity || 1;
-
-		var params = {
-			recieptId: recieptId,
-			sku: sku,
-			name: name,
-			quantity: quantity,
-			unitPrice: price,
-			revenue: quantity * price,
-			currency: currency || 'USD'
-		}
+	this.trackPurchase = function(price, purchaseData, dataSignature, token){
+		var params = {"price": price,
+					  "purchaseData": purchaseData,
+					  "dataSignature": dataSignature,
+					  "token": token};
 
 		pluginSend("trackPurchase", params);
 	}
-	// level can be anything like milestone, ms-1
-	// attr something that defines level
-	this.trackLevel = function (level, attr) {
-		var params = {
-			level: level,
-			attr: attr
-		}
-		pluginSend("trackLevel", params);
+
+	this.trackOpen = function() {
+		pluginSend("trackOpen");
+	}
+
+	this.setUID = function(uid) {
+		var params = {"uid": uid};
+
+		pluginSend("setUID", params);
 	}
 });
 
