2b307a7062c135f857a31c184662923e6f4306ea
Vinod CG <vini@live.in>
2014-09-01 11:49:28 +0530

    fix indentation

--- android/HasoffersPlugin.java
+++ android/HasoffersPlugin.java
@@ -45,7 +45,7 @@ public class HasoffersPlugin implements IPlugin {
 
   public void onCreate(Activity passedActivity, Bundle savedInstanceState) {
     activity = passedActivity;
-        //Below segment gets the hasoffers key
+    //Below segment gets the hasoffers key
     PackageManager manager = activity.getPackageManager();
     String hasoffersKey = "";
     String hasoffersAdvId = "";
@@ -58,7 +58,7 @@ public class HasoffersPlugin implements IPlugin {
     } catch (Exception e) {
       android.util.Log.d("EXCEPTION", "" + e.getMessage());
     }
-        //Init MAT Tracker
+    //Init MAT Tracker
     MobileAppTracker.init(
       this.ctx,
       hasoffersAdvId,
@@ -68,124 +68,124 @@ public class HasoffersPlugin implements IPlugin {
 
     new Thread(new Runnable() {
       @Override public void run() {
-          // See sample code at http://developer.android.com/google/play-services/id.html
+        // See sample code at http://developer.android.com/google/play-services/id.html
         try {
           Info adInfo = AdvertisingIdClient.getAdvertisingIdInfo(ctx);
           _mobileapptracker.setGoogleAdvertisingId(adInfo.getId(), adInfo.isLimitAdTrackingEnabled());
         } catch (IOException e) {
-              // Unrecoverable error connecting to Google Play services (e.g.,
-              // the old version of the service doesn't support getting AdvertisingId).
+          // Unrecoverable error connecting to Google Play services (e.g.,
+          // the old version of the service doesn't support getting AdvertisingId).
           _mobileapptracker.setAndroidId(Secure.getString(activity.getContentResolver(), Secure.ANDROID_ID));
         } catch (GooglePlayServicesNotAvailableException e) {
-              // Google Play services is not available entirely.
+          // Google Play services is not available entirely.
           _mobileapptracker.setAndroidId(Secure.getString(activity.getContentResolver(), Secure.ANDROID_ID));
         } catch (GooglePlayServicesRepairableException e) {
-              // Encountered a recoverable error connecting to Google Play services.
+          // Encountered a recoverable error connecting to Google Play services.
           _mobileapptracker.setAndroidId(Secure.getString(activity.getContentResolver(), Secure.ANDROID_ID));
         } catch (NullPointerException e) {
-              // getId() is sometimes null
+          // getId() is sometimes null
           _mobileapptracker.setAndroidId(Secure.getString(activity.getContentResolver(), Secure.ANDROID_ID));
         }
       }
     }).start();
 
-        //Disallow these in production build
-        //_mobileapptracker.setAllowDuplicates(true);
-        //_mobileapptracker.setDebugMode(true);
-}
-
-public void onResume() {
-  logger.log(TAG, "Measuring session now");
-  _mobileapptracker.setReferralSources(activity);
-  _mobileapptracker.measureSession();
-}
-
-public void onStart() {
-}
-// TODO: Error Handling code
-public void setUserIds(String json) {
-  try {
-    JSONObject data = new JSONObject();
-    if (data.has("uid")) {
-     _mobileapptracker.setUserId(data.getString("uid"));
-   }
-   if (data.has("fb_id")) {
-     _mobileapptracker.setFacebookUserId(data.getString("fb_id"));
-   }
-   if (data.has("google_id")) {
-    _mobileapptracker.setGoogleUserId(data.getString("google_id"));
+    //Disallow these in production build
+    //_mobileapptracker.setAllowDuplicates(true);
+    //_mobileapptracker.setDebugMode(true);
   }
-  if (data.has("twitter_id")) {
-    _mobileapptracker.setTwitterUserId(data.getString("twitter_id"));
-  }
-} catch (JSONException ex) {
-  ex.printStackTrace();
-}
-}
 
-public void trackLevel (String json) {
-  try {
-    JSONObject data = new JSONObject(json);
-    if (data.has("level")) {
-      _mobileapptracker.setEventLevel(data.getInt("level"));
-    } else {
-      throw new Exception("No Level data found while setting a level");
+  public void onResume() {
+    logger.log(TAG, "Measuring session now");
+    _mobileapptracker.setReferralSources(activity);
+    _mobileapptracker.measureSession();
+  }
+
+  public void onStart() {
+  }
+  // TODO: Error Handling code
+  public void setUserIds(String json) {
+    try {
+      JSONObject data = new JSONObject();
+      if (data.has("uid")) {
+        _mobileapptracker.setUserId(data.getString("uid"));
+      }
+      if (data.has("fb_id")) {
+        _mobileapptracker.setFacebookUserId(data.getString("fb_id"));
+      }
+      if (data.has("google_id")) {
+        _mobileapptracker.setGoogleUserId(data.getString("google_id"));
+      }
+      if (data.has("twitter_id")) {
+        _mobileapptracker.setTwitterUserId(data.getString("twitter_id"));
+      }
+    } catch (JSONException ex) {
+      ex.printStackTrace();
     }
-    if (data.has("attr")) {
-      _mobileapptracker.setEventAttribute1(data.getString("attr"));
+  }
+
+  public void trackLevel (String json) {
+    try {
+      JSONObject data = new JSONObject(json);
+      if (data.has("level")) {
+        _mobileapptracker.setEventLevel(data.getInt("level"));
+      } else {
+        throw new Exception("No Level data found while setting a level");
+      }
+      if (data.has("attr")) {
+        _mobileapptracker.setEventAttribute1(data.getString("attr"));
+      }
+      _mobileapptracker.measureAction("level_achieved");
+    } catch (Exception e) {
+      e.printStackTrace();
     }
-    _mobileapptracker.measureAction("level_achieved");
-  } catch (Exception e) {
-    e.printStackTrace();
+
   }
 
-}
-
-public void trackPurchase(String json) {
-  try {
-    JSONObject data = new JSONObject(json);
-    String recieptId = data.getString("recieptId");
-    String sku = data.getString("sku");
-    String name = data.getString("name");
-    int quantity = data.getInt("quantity");
-    double revenue = data.getDouble("revenue");
-    double unitPrice = data.getDouble("unitPrice");
-    List<MATEventItem> events = new ArrayList();
-    events.add(new MATEventItem(name, quantity, unitPrice, revenue));
-    String currency = data.getString("currency");
-    _mobileapptracker.measureAction("purchase", events, revenue, currency, recieptId);
-    logger.log(TAG, "Sent payment events", recieptId, sku, name, quantity, revenue, unitPrice);
-  } catch (JSONException ex) {
-    ex.printStackTrace();
+  public void trackPurchase(String json) {
+    try {
+      JSONObject data = new JSONObject(json);
+      String recieptId = data.getString("recieptId");
+      String sku = data.getString("sku");
+      String name = data.getString("name");
+      int quantity = data.getInt("quantity");
+      double revenue = data.getDouble("revenue");
+      double unitPrice = data.getDouble("unitPrice");
+      List<MATEventItem> events = new ArrayList();
+      events.add(new MATEventItem(name, quantity, unitPrice, revenue));
+      String currency = data.getString("currency");
+      _mobileapptracker.measureAction("purchase", events, revenue, currency, recieptId);
+      logger.log(TAG, "Sent payment events", recieptId, sku, name, quantity, revenue, unitPrice);
+    } catch (JSONException ex) {
+      ex.printStackTrace();
+    }
   }
-}
 
-public void onPause() {
+  public void onPause() {
 
-}
+  }
 
-public void onStop() {
-}
+  public void onStop() {
+  }
 
-public void onDestroy() {
-}
+  public void onDestroy() {
+  }
 
-public void onNewIntent(Intent intent) {
+  public void onNewIntent(Intent intent) {
 
-}
+  }
 
-public void setInstallReferrer(String referrer) {
+  public void setInstallReferrer(String referrer) {
 
-}
+  }
 
-public void onActivityResult(Integer request, Integer result, Intent data) {
+  public void onActivityResult(Integer request, Integer result, Intent data) {
 
-}
+  }
 
-public boolean consumeOnBackPressed() {
-  return true;
-}
+  public boolean consumeOnBackPressed() {
+    return true;
+  }
 
-public void onBackPressed() {
-}
+  public void onBackPressed() {
+  }
 }
--- android/manifest.xsl
+++ android/manifest.xsl
@@ -1,23 +1,23 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform" xmlns:android="http://schemas.android.com/apk/res/android">
 
-	<xsl:param name="hasofferskey" />
-	<xsl:param name="hasoffersadvid" />
+  <xsl:param name="hasofferskey" />
+  <xsl:param name="hasoffersadvid" />
 
-	<xsl:output indent="yes" />
-	<xsl:template match="comment()" />
+  <xsl:output indent="yes" />
+  <xsl:template match="comment()" />
 
-	<xsl:template match="meta-data[@android:name='HASOFFERS_KEY']">
-		<meta-data android:name="HASOFFERS_KEY" android:value="{$hasofferskey}"/>
-	</xsl:template>
+  <xsl:template match="meta-data[@android:name='HASOFFERS_KEY']">
+    <meta-data android:name="HASOFFERS_KEY" android:value="{$hasofferskey}"/>
+  </xsl:template>
 
-	<xsl:template match="meta-data[@android:name='HASOFFERS_ADV_ID']">
-		<meta-data android:name="HASOFFERS_ADV_ID" android:value="\ {$hasoffersadvid}"/>
-	</xsl:template>
+  <xsl:template match="meta-data[@android:name='HASOFFERS_ADV_ID']">
+    <meta-data android:name="HASOFFERS_ADV_ID" android:value="\ {$hasoffersadvid}"/>
+  </xsl:template>
 
-	<xsl:template match="@*|node()">
-		<xsl:copy>
-			<xsl:apply-templates select="@*|node()" />
-		</xsl:copy>
-	</xsl:template>
+  <xsl:template match="@*|node()">
+    <xsl:copy>
+      <xsl:apply-templates select="@*|node()" />
+    </xsl:copy>
+  </xsl:template>
 </xsl:stylesheet>
