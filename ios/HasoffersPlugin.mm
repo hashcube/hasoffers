49a246abe4cf5e157fad2c2aa5b82fffc44fabcd
Ramprasad Rajendran <r.ramprasad@gmail.com>
2014-09-02 12:01:29 +0530

    Tabs -> 2 Spaces

--- ios/HasoffersPlugin.mm
+++ ios/HasoffersPlugin.mm
@@ -4,101 +4,101 @@
 
 // The plugin must call super dealloc.
 - (void) dealloc {
-	[super dealloc];
+  [super dealloc];
 }
 
 // The plugin must call super init.
 - (id) init {
-	self = [super init];
-	if (!self) {
-		return nil;
-	}
+  self = [super init];
+  if (!self) {
+    return nil;
+  }
 
-	return self;
+  return self;
 }
 
 - (void) initializeWithManifest:(NSDictionary *)manifest appDelegate:(TeaLeafAppDelegate *)appDelegate {
-	@try {
+  @try {
 
-		NSDictionary *ios = [manifest valueForKey:@"ios"];
-		NSString * const MAT_ADVERTISER_ID = [ios valueForKey:@"hasoffersadvid"];
-		NSString * const MAT_CONVERSION_KEY = [ios valueForKey:@"hasofferskey"];
+    NSDictionary *ios = [manifest valueForKey:@"ios"];
+    NSString * const MAT_ADVERTISER_ID = [ios valueForKey:@"hasoffersadvid"];
+    NSString * const MAT_CONVERSION_KEY = [ios valueForKey:@"hasofferskey"];
 
-		if (MAT_ADVERTISER_ID && MAT_CONVERSION_KEY) {
-			[[MobileAppTracker sharedManager] startTrackerWithMATAdvertiserId:MAT_ADVERTISER_ID
-				MATConversionKey:MAT_CONVERSION_KEY];
-		}
+    if (MAT_ADVERTISER_ID && MAT_CONVERSION_KEY) {
+      [[MobileAppTracker sharedManager] startTrackerWithMATAdvertiserId:MAT_ADVERTISER_ID
+                                                       MATConversionKey:MAT_CONVERSION_KEY];
+    }
 
-		// FOR DEBUG ONLY: Turn this on to see debug messages
-		//[[MobileAppTracker sharedManager] setDebugMode:YES];
+    // FOR DEBUG ONLY: Turn this on to see debug messages
+    //[[MobileAppTracker sharedManager] setDebugMode:YES];
 
-		// FOR DEBUG ONLY: Turn this on to allow duplicate events
-		//[[MobileAppTracker sharedManager] setAllowDuplicateRequests:YES];
+    // FOR DEBUG ONLY: Turn this on to allow duplicate events
+    //[[MobileAppTracker sharedManager] setAllowDuplicateRequests:YES];
 
-		// set this class as the delegate for MAT callbacks
-		//[[MobileAppTracker sharedManager] setDelegate:self];
+    // set this class as the delegate for MAT callbacks
+    //[[MobileAppTracker sharedManager] setDelegate:self];
 
-	}
-	@catch (NSException *exception) {
-		NSLog(@"{facebook} Exception while initializing: %@", exception);
-	}
+  }
+  @catch (NSException *exception) {
+    NSLog(@"{facebook} Exception while initializing: %@", exception);
+  }
 }
 
 /*
-#pragma mark - MobileAppTrackerDelegate Methods
+  #pragma mark - MobileAppTrackerDelegate Methods
 // MAT tracking request success callback
 - (void)mobileAppTracker:(MobileAppTracker *)tracker didSucceedWithData:(id)data
 {
-	NSString *response = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
-	NSLog(@"MAT.success: %@", response);
+NSString *response = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+NSLog(@"MAT.success: %@", response);
 }
 
 // MAT tracking request failure callback
 - (void)mobileAppTracker:(MobileAppTracker *)tracker didFailWithError:(NSError *)error
 {
-	NSLog(@"MAT.failure: %@", error);
+NSLog(@"MAT.failure: %@", error);
 }
 */
 
 - (void) setUID:(NSDictionary *)jsonObject {
-	if([jsonObject objectForKey:@"uid"]) {
-		NSString *uid = [NSString stringWithFormat:@"%@",[jsonObject valueForKey:@"uid"]];
-		[[MobileAppTracker sharedManager] setUserId:uid];
-	}
+  if([jsonObject objectForKey:@"uid"]) {
+    NSString *uid = [NSString stringWithFormat:@"%@",[jsonObject valueForKey:@"uid"]];
+    [[MobileAppTracker sharedManager] setUserId:uid];
+  }
 }
 
 - (void) trackInstall:(NSDictionary *)jsonObject {
-	if([jsonObject objectForKey:@"userType"]) {
-		NSString *userType = [NSString stringWithFormat:@"%@",[jsonObject valueForKey:@"userType"]];
-		if([userType  isEqual: @"old"]) {
-			[[MobileAppTracker sharedManager] trackUpdate];
-		} else {
-			[[MobileAppTracker sharedManager] trackInstall];
-		}
-	}
+  if([jsonObject objectForKey:@"userType"]) {
+    NSString *userType = [NSString stringWithFormat:@"%@",[jsonObject valueForKey:@"userType"]];
+    if([userType  isEqual: @"old"]) {
+      [[MobileAppTracker sharedManager] trackUpdate];
+    } else {
+      [[MobileAppTracker sharedManager] trackInstall];
+    }
+  }
 }
 
 - (void) trackPurchase:(NSDictionary *)jsonObject {
-	NSData *data=[NSKeyedArchiver archivedDataWithRootObject:[jsonObject valueForKey:@"dataSignature"]];
-	[[MobileAppTracker sharedManager] trackActionForEventIdOrName:@"purchase"
-		eventIsId:NO
-		eventItems:  @[[MATEventItem eventItemWithName:[NSString stringWithFormat:@"%@",[jsonObject valueForKey:@"purchaseData"]]
-		unitPrice:[[jsonObject valueForKey:@"price"] floatValue]
-		quantity:1
-		revenue:[[jsonObject valueForKey:@"price"] floatValue]
-		attribute1:@"attr1" 
-		attribute2:@"attr2" 
-		attribute3:@"attr3" 
-		attribute4:@"attr4" 
-		attribute5:@"attr5"]]
-		referenceId:[NSString stringWithFormat:@"%@", [jsonObject valueForKey:@"token"]]
-		revenueAmount:[[jsonObject valueForKey:@"price"] floatValue]
-		currencyCode:@"USD"
-		transactionState:1
-		receipt:data];
+  NSData *data=[NSKeyedArchiver archivedDataWithRootObject:[jsonObject valueForKey:@"dataSignature"]];
+  [[MobileAppTracker sharedManager] trackActionForEventIdOrName:@"purchase"
+                                                      eventIsId:NO
+                                                     eventItems:  @[[MATEventItem eventItemWithName:[NSString stringWithFormat:@"%@",[jsonObject valueForKey:@"purchaseData"]]
+                                                                                          unitPrice:[[jsonObject valueForKey:@"price"] floatValue]
+                                                                                           quantity:1
+                                                                                            revenue:[[jsonObject valueForKey:@"price"] floatValue]
+                                                                                         attribute1:@"attr1"
+                                                                                         attribute2:@"attr2"
+                                                                                         attribute3:@"attr3"
+                                                                                         attribute4:@"attr4"
+                                                                                         attribute5:@"attr5"]]
+                                                    referenceId:[NSString stringWithFormat:@"%@", [jsonObject valueForKey:@"token"]]
+                                                  revenueAmount:[[jsonObject valueForKey:@"price"] floatValue]
+                                                   currencyCode:@"USD"
+                                               transactionState:1
+                                                        receipt:data];
 }
 
 - (void) trackOpen:(NSDictionary *)dummy {
-	[[MobileAppTracker sharedManager] trackActionForEventIdOrName:@"open" eventIsId:NO];
+  [[MobileAppTracker sharedManager] trackActionForEventIdOrName:@"open" eventIsId:NO];
 }
 @end
