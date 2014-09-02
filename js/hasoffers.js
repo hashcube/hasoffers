a40017744862b041b0470a9cdbba90643ffe8dea
Vinod CG <vini@live.in>
2014-09-02 11:38:24 +0530

    change to space indentation

--- js/hasoffers.js
+++ js/hasoffers.js
@@ -3,49 +3,49 @@
  */
 
 function pluginSend(evt, params) {
-	NATIVE && NATIVE.plugins && NATIVE.plugins.sendEvent &&
-		NATIVE.plugins.sendEvent("HasoffersPlugin", evt,
-				JSON.stringify(params || {}));
+  NATIVE && NATIVE.plugins && NATIVE.plugins.sendEvent &&
+    NATIVE.plugins.sendEvent("HasoffersPlugin", evt,
+        JSON.stringify(params || {}));
 }
 
 var Hasoffers = Class(function () {
-	this.init = function(){
-		logger.log("{hasoffers} Registering for events on startup");
-	}
+  this.init = function(){
+    logger.log("{hasoffers} Registering for events on startup");
+  }
 
-	this.setUserIds = function (params) {
-		// Allowed params
-		// uid : custom user id
-		// fb_id: facebook user ID
-		// google_id: Google ID if login is based on google
-		// twitter_id: Twitter ID
-		pluginSend("setUserIds", params);
-	}
+  this.setUserIds = function (params) {
+    // Allowed params
+    // uid : custom user id
+    // fb_id: facebook user ID
+    // google_id: Google ID if login is based on google
+    // twitter_id: Twitter ID
+    pluginSend("setUserIds", params);
+  }
 
-	this.trackPurchase = function(receipt, sku, name, price, quantity, currency){
-		quantity = quantity || 1;
+  this.trackPurchase = function(receipt, sku, name, price, quantity, currency){
+    quantity = quantity || 1;
 
-		var params = {
-			receipt: receipt,
-			sku: sku,
-			name: name,
-			quantity: quantity,
-			unitPrice: price,
-			revenue: quantity * price,
-			currency: currency || 'USD'
-		}
+    var params = {
+      receipt: receipt,
+      sku: sku,
+      name: name,
+      quantity: quantity,
+      unitPrice: price,
+      revenue: quantity * price,
+      currency: currency || 'USD'
+    }
 
-		pluginSend("trackPurchase", params);
-	}
-	// level can be anything like milestone, ms-1
-	// attr something that defines level
-	this.trackLevel = function (level, attr) {
-		var params = {
-			level: level,
-			attr: attr
-		}
-		pluginSend("trackLevel", params);
-	}
+    pluginSend("trackPurchase", params);
+  }
+  // level can be anything like milestone, ms-1
+  // attr something that defines level
+  this.trackLevel = function (level, attr) {
+    var params = {
+      level: level,
+      attr: attr
+    }
+    pluginSend("trackLevel", params);
+  }
 });
 
 exports = new Hasoffers();
